21ab2a0600eb738f6bcb20e34a9f18e44b3b9f10
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2025-04-20 08:05:28 +0700

    fix

--- wikidata-chat/backend/wikidata_web_halo/urls.py
+++
@@ -1,8 +0,0 @@
-# backend/wikidata_web/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chat.urls')),
-]
